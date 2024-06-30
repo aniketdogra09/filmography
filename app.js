a522850e9bd9c08b5c08d47a10ed4703fe81f170
aniketdogra09 <aniketdogra09@gmail.com>
2024-06-30 22:27:27 +0530

    commit 6
    the page now displays almost all necessary movie-details that the HTML page has to offer. Slight changes are yet to be made - in genre, a section of awards to be added and removing 'my rating' and 'menu' until functional.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    
+
     const api="https://www.omdbapi.com/?apikey=e061ebe7&t=";
     const searchButton=document.getElementById('search-button-id');
 
@@ -8,11 +8,30 @@ document.addEventListener('DOMContentLoaded',()=>{
         const movieName=document.getElementById('search-box');
         const query=api+movieName.value;
         const movieTitle=document.getElementById('movie-title');
+        const releaseYear=document.getElementById('released-in');
+        const ratedAs=document.getElementById('rated');
+        const movieRuntime=document.getElementById('runtime');
+        const movieImdbRating=document.getElementById('imdb-rating');
+        const moviePlot=document.getElementById('movie-plot-id');
+        const movieDirector=document.getElementById('director');
+        const movieWriters=document.getElementById('writers');
+        const movieActors=document.getElementById('actors');
+        const moviePoster=document.getElementById('poster-image-id');
 
         fetch(query).then((data)=>{
             return data.json();
         }).then((data)=>{
             movieTitle.innerText=data.Title;
+            releaseYear.innerText=data.Year;
+            ratedAs.innerText=data.Rated;
+            releaseYear.innerText=data.Year;
+            movieRuntime.innerText=data.Runtime;
+            movieImdbRating.innerText=data.imdbRating+"/10";
+            moviePlot.innerText=data.Plot;
+            movieDirector.innerText=data.Director;
+            movieWriters.innerText=data.Writer;
+            movieActors.innerText=data.Actors;
+            moviePoster.src=data.Poster;
         });
     });
 
